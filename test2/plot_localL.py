2a6dc1f7ad71ac922877081ed086a4199c6035c0
Michael Shih <sc2.shih@gmail.com>
2018-11-12 17:34:45 -0600

    good

--- test2/plot_localL.py
+++ test2/plot_localL.py
@@ -3,6 +3,9 @@ import os, sys
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy import linspace, meshgrid
+from matplotlib.mlab import griddata
+
 
 path = '/Volumes/LaCie_DataStorage/xiaochao_wei_STORM imaging/STORM_imaging'
 analysis_dir = 'analysis'
@@ -29,15 +32,31 @@ for directory, dir_names, file_names in os.walk(inputfilepath):
 print(filelist)
 print(fileabslist)
 
+
+def grid(x, y, z, resX=100, resY=100):
+    "Convert 3 column data to matplotlib grid"
+    xi = linspace(min(x), max(x), resX)
+    yi = linspace(min(y), max(y), resY)
+    Z = griddata(x, y, z, xi, yi, interp='linear')
+    X, Y = meshgrid(xi, yi)
+    return X, Y, Z
+
 # %%
 for i in range(len(filelist)):
     filepath = fileabslist[i]
     # print(filepath)
     data = pd.read_csv(filepath, header=0)
-    
     #print(data)
     
-    fig = plt.figure()
-    plt.scatter(data['x'], data['y'], c = data['z'], size = 0.2)
-    fig.savefig(outputfilelist_DM[i])
+    fig = plt.figure(figsize = (5,10))
+    
+    plt.subplot(211)
+    plt.scatter(data['x'], data['y'], c = data['z'], s = 1)
+
+    plt.subplot(212)
+    X, Y, Z = grid(data['x'], data['y'], data['z'])
+    plt.contourf(X, Y, Z)
+    
+    plt.show
+
     break
