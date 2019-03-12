83c18071640b6200ae59b6a4ba95408373faf05d
Michael Shih <sc2.shih@gmail.com>
2019-03-12 12:45:24 -0500

    csvdata_sliced first

--- csv_slicer.py
+++ csv_slicer.py
@@ -5,52 +5,120 @@ import os, sys
 import pandas as pd
 # from tkinter import *
 
+# Functions Section Begins ----------------------------------------------------- #
+def dircheck(targetpaths):
+	"""
+	dircheck checks the target folder and create the folder if it does not exist.
+	targetdirlist: list of folderpath
+	"""
+	# print(type(targetpaths))
+	if isinstance(targetpaths, str): 
+		print(os.path.exists(targetpaths))
+		if not os.path.exists(targetpaths):
+			os.makedirs(targetpaths)
+	elif isinstance(targetpaths, list): 
+		for path in targetpaths:
+			if not os.path.exists(path):
+				os.makedirs(path)
+
+def listfiles(path, extension = None):
+	filelist = []
+	fileabslist = []
+	for directory, dir_names, file_names in os.walk(path):
+		# print(file_names)
+		
+		for file_name in file_names:
+			if (not file_name.startswith('.')) & (file_name.endswith(extension)):
+				file_name_base = file_name.replace(extension, '')
+				filepath_tmp =  os.path.join(directory, file_name)
+				filelist.append(file_name_base)
+				fileabslist.append(filepath_tmp)
+	
+	return {'filelist': filelist,
+			'fileabslist': fileabslist}
+
+def getpendinglist(src_dir, op_dir, src_ext = '.nd2', op_ext = '.csv'):
+	"""
+	getpendinglist compares the files from src_dir and the accomplisjed file in op_dir, 
+	then creates a pending list of unprocessed image. 
+	"""
+	
+	srclist = listfiles(src_dir, src_ext)
+	srclist = srclist['fileabslist']
+	oplist = listfiles(op_dir, op_ext)
+	oplist = oplist['fileabslist']
+
+	oplist_basename = []
+	for i in oplist:
+		name = os.path.basename(i)
+		print('name: {}'.format(name))
+		basename = os.path.splitext(name)[0]
+		print('basename: {}'.format(basename))
+		oplist_basename.append(basename)
+	
+	pendingfllist = []
+	pendingpathlist_input = []
+	pendingpathlist_output = []
+	
+	for i in range(len(srclist)):
+		srcflname = os.path.basename(srclist[i])
+		srcflbasename = os.path.splitext(srcflname)[0]
+		
+		if not srcflbasename in oplist_basename:
+			pendingfllist.append(srcflbasename)
+			pendingpathlist_input.append(srclist[i])			
+			pendingpathlist_output.append(os.path.join(op_dir, srcflbasename + op_ext))
+			
+	return (pendingfllist, pendingpathlist_input, pendingpathlist_output)
+# Functions Section Ends ----------------------------------------------------- #
+
 # create input path
 # load the csv file
 path = '/Volumes/LaCie_DataStorage/xiaochao_wei_STORM imaging/STORM_imaging'
-analysis_dir = 'analysis_012019'
-csvdatadir = 'csvdata'
-filepath = os.path.join(path, analysis_dir, csvdatadir)
+analysis_dir = 'analysis_20190308'
+analysis_subdir = 'tstorm'
+csvdata_dir = 'csvdata'
+nchannel = 2 
 
-filelist = []
-fileabslist = []
-for directory, dir_names, file_names in os.walk(filepath):
-	for file_name in file_names:
-		if (not file_name.startswith('.')) & (file_name.endswith('.csv')):
-			filepath_tmp =  os.path.join(directory, file_name)
-			filelist.append(file_name)
-			fileabslist.append(filepath_tmp)
-
-print(filelist)
-print(fileabslist)
+ip_path = os.path.join(path, analysis_dir, analysis_subdir, csvdata_dir)
 
 # create output path
-outputfolder = 'csvdata_sliced'
-outputfilepath = os.path.join(path, analysis_dir, outputfolder)
-if not os.path.exists(outputfilepath):
-		os.makedirs(outputfilepath)
+dir_for_check = []
+op_dir = 'csvdata_sliced'
+op_path = os.path.join(path, analysis_dir, analysis_subdir, op_dir)
+dir_for_check.append(op_path)
+for i in range(nchannel):
+	dir_tmp = os.path.join(op_path, str(i+1))
+	dir_for_check.append(dir_tmp)
+dircheck(dir_for_check)
 
-outputfilelist = []
-outputfileabslist = []
-for directory, dir_names, file_names in os.walk(filepath):
-	for file_name in file_names:
-		if (not file_name.startswith('.')) & (file_name.endswith('.csv')):
-			filepath_tmp =  os.path.join(outputfilepath, file_name)
-			outputfilelist.append(file_name)
-			outputfileabslist.append(filepath_tmp)
+# %%
+# prepare input file list
+pendingfllist = []
+pendingpathlist_input = []
+pendingpathlist_output = []
+for i in range(nchannel):
+	src_dir = os.path.join(ip_path, str(i+1))
+	op_dir = os.path.join(op_path, str(i+1))
+	pendingfllist_tmp, pendingpathlist_input_tmp, pendingpathlist_output_tmp = getpendinglist(src_dir, op_dir, src_ext = '.csv', op_ext = '.csv')
+	pendingfllist = pendingfllist + pendingfllist_tmp
+	pendingpathlist_input = pendingpathlist_input + pendingpathlist_input_tmp
+	pendingpathlist_output = pendingpathlist_output + pendingpathlist_output_tmp
+# print(pendingfllist)
+print(pendingpathlist_input)
+print(pendingpathlist_output)
 
-print(outputfileabslist)
 
 
 # %%
 # define the range of time frame
-frame_start = 5000
+frame_start = 7000
 frame_end = 10001
 
 # slice the csv file
 #for i in range(1):
-for i in range(len(filelist)):
-    filepath = fileabslist[i]
+for i in range(len(pendingfllist)):
+    filepath = pendingpathlist_input[i]
     data = pd.read_csv(filepath, header=0)
     data = pd.DataFrame(data)
     print(data.head())
@@ -59,6 +127,6 @@ for i in range(len(filelist)):
     print(data_sliced.head())
     print(data_sliced.tail())
     
-    print(outputfileabslist[i])
-    data_sliced.to_csv(outputfileabslist[i], index = False)
+    print(pendingpathlist_output[i])
+    data_sliced.to_csv(pendingpathlist_output[i], index = False)
     
---
+++ plot_intensity.py
@@ -0,0 +1,95 @@
+# %%
+# make plot
+import os, sys
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.style
+import matplotlib as mpl
+mpl.style.use('default')
+
+# Functions Section Begins ----------------------------------------------------- #
+def dircheck(targetpaths):
+	"""
+	dircheck checks the target folder and create the folder if it does not exist.
+	targetdirlist: list of folderpath
+	"""
+	# print(type(targetpaths))
+	if isinstance(targetpaths, str): 
+		print(os.path.exists(targetpaths))
+		if not os.path.exists(targetpaths):
+			os.makedirs(targetpaths)
+	elif isinstance(targetpaths, list): 
+		for path in targetpaths:
+			if not os.path.exists(path):
+				os.makedirs(path)
+            
+# Functions Section Ends ----------------------------------------------------- #
+# %%
+# load the csv file
+path = '/Volumes/LaCie_DataStorage/xiaochao_wei_STORM imaging/STORM_imaging'
+ippath = os.path.join(path, 'analysis_20190308', 'preprocessing', 'imgintensity')
+oppath = os.path.join(path, 'analysis_20190308', 'preprocessing', 'imginfo', 'plot')
+
+# %%
+# merge the csv file
+filelist = os.listdir(ippath)
+print(filelist)
+
+def testgroup(x):
+    if x == 'w':
+        return 'wildtype'
+    elif x == 'k': 
+        return 'knockout'
+
+list_data = []
+for filename in filelist:
+    df = pd.read_csv(os.path.join(ippath, filename), index_col=None, header=0)
+    df['file_name'] = filename
+    df['img_name'] = filename.replace('.csv', '')
+    test = df['file_name'].str.extract(r'^\d{4}_\d{2}_\d{2}_([a-z])[0-9].csv')
+    df['group'] = test[0].apply(lambda x : testgroup(x))
+    
+    list_data.append(df)
+
+data = pd.concat(list_data, axis = 0, ignore_index=True)
+
+# %%
+display(data.head())
+
+print(data.shape)
+
+# %%
+imgnamecreator = lambda x: x.replace('.csv', '')
+filelist_imgname = list(map(imgnamecreator, filelist))
+print(filelist_imgname)
+
+# %% 
+data_temp = data.loc[data['img_name'] == filelist_imgname[0]]
+display(data_temp)
+
+# %%
+labels_group, uni_group = pd.factorize(data['group'])
+labels_ch, uni_ch = pd.factorize(data['channel'])
+print(uni_group)
+
+# %%
+dircheck(oppath)
+
+for group in uni_group:
+    data_group = data[data['group'] == group]
+    imgname_labels, imgname_uni = pd.factorize(data_group['img_name'])
+    print(imgname_uni)
+    fig, axes = plt.subplots(ncols = 4, nrows = 5)
+    for i, ax in enumerate(axes.flatten()):
+        if i < len(imgname_uni):
+            data_temp = data_group.loc[data_group['img_name'] == imgname_uni[i]]
+            for j in uni_ch:
+                df = data_temp[data_temp['channel'] == j]
+                ax.plot(df['frame'], df['mean'])
+            # ax.set_title('{}'.format(filelist_imgname[i]))
+            ax.set_ylim(0, 20000)
+    fig.set_figheight(10)
+    fig.set_figwidth(10)
+    fig.tight_layout()   
+    plt.show()
+    fig.savefig(os.path.join(oppath, group))
