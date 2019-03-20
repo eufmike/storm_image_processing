ecd1119ea97e005177077f77427fb4a15115ba3d
Michael Shih <sc2.shih@gmail.com>
2019-03-20 18:50:28 -0500

    CBC done

---
+++ Fiji/CBC_latest.py
@@ -0,0 +1,239 @@
+#@ UIService uiService
+#@ LogService log
+#@ File(label="Select a directory", style="directory", value="/Volumes/LaCie_DataStorage/xiaochao_wei_STORM imaging/STORM_imaging", persist=false) path
+#@ String(label="Folder for output", value = "analysis_20190308", persist=false) dir_output
+#@ String(label="Folder for raw images", value = "resource", persist=false) dir_srcimg
+#@ String(label="Folder for raw images", value = "03_testdata", persist=false) subdir_srcimg
+#@ String(label="Folder for tif images", value = "preproimg", persist=false) dir_preproimg
+#@ String(label="Folder for input .csv", value = "tstorm", persist=false) ip_dir
+#@ String(label="Subfolder for input .csv", value = "csvdata_crop", persist=false) ip_subdir
+#@ String(label="Folder for CBC output .csv", value = "spacial_test", persist=false) op_dir
+#@ String(label="Subfolder for CBC output .csv", value = "CBC_results", persist=false) op_subdir
+#@ Boolean(label="Batchmode", value=false, persist=true) batchmodeop
+
+
+# For ImageJ/Fiji
+import sys
+import os
+import re
+import csv
+import gc
+import time
+from ij import IJ
+from ij import plugin
+from ij import gui
+from ij.io import FileSaver
+from ij import WindowManager as wm
+from ij.process import ImageStatistics as IS 
+
+# Import JAVA modules
+import java.awt.Color as Color
+import java.lang.System.gc as javagc
+
+# Import Bio-Formats
+from loci.plugins import BF
+from loci.plugins.in import ImporterOptions
+
+# Functions Section Begins ----------------------------------------------------- #
+print('Loading functions ...')
+# log.info('Loading functions ...')
+
+def filesavercheck(fs, outputdir, filename):
+	
+	"""
+	FileSaverCheck first check if the input folder exists, 
+	then check if the input file exists in the targer folder
+	
+	If the folder does not exist, it returns error message:
+	"Folder does not exist or it's not a folder!"
+	If the folder exists but the file also exists:
+	File exists! Not saving the image, would overwrite a file!"
+	Otherwise, it will save file and return message: 
+	"File saved successfully at given filepath"
+	 
+	Arguments:
+	outputdir: output directory
+	filename: input file with absolute path
+	"""
+	
+	if os.path.exists(outputdir) and os.path.isdir(outputdir):  
+  		print "folder exists:", outputdir
+  		filepath = os.path.join(outputdir, filename) # Operating System-specific  
+  		if os.path.exists(filepath):
+  			print "File exists! Not saving the image, would overwrite a file!"  
+  		elif fs.saveAsTiff(filepath):
+  			print "File saved successfully at ", filepath  
+	else:  
+  		print "Folder does not exist or it's not a folder!" 
+  		
+def dircheck(targetpaths):
+	"""
+	dircheck checks the target folder and create the folder if it does not exist.
+	targetdirlist: list of folderpath
+	"""
+	# print(type(targetpaths))
+	if type(targetpaths) is unicode: 
+		print(os.path.exists(targetpaths))
+		if not os.path.exists(targetpaths):
+			os.makedirs(targetpaths)
+	elif type(targetpaths) is list: 
+		for path in targetpaths:
+			if not os.path.exists(path):
+				os.makedirs(path)
+
+def getprocessedimg(op_dir, pattern = r'(.+?).'):
+	"""
+	NOT USING
+	getprocessedimg check the output folder and create a list of processed data
+	pattern: the pattern re search
+	"""
+	processed_img = []
+	for (directory, dir_names, file_names) in os.walk(op_dir):
+		for file_name in file_names:
+			# print(file_name)
+
+			# search the processed files by using re.search
+			m = re.search(pattern, file_name)
+			if m: 
+				# print(m)
+				file_name_temp = m.group(1)
+				processed_img.append(file_name_temp)
+	# replace the duplicated filenames
+	processed_img = list(set(processed_img))
+	
+	return (processed_img)
+
+def listfiles(path, extension = None):
+	filelist = []
+	fileabslist = []
+	for directory, dir_names, file_names in os.walk(path):
+		# print(file_names)
+		
+		for file_name in file_names:
+			if (not file_name.startswith('.')) & (file_name.endswith(extension)):
+				filepath_tmp =  os.path.join(directory, file_name + extension)
+				filelist.append(file_name)
+				fileabslist.append(filepath_tmp)
+	
+	return {'filelist': filelist,
+			'fileabslist': fileabslist}
+	
+# def getpendinglist(src_dir, op_dir, src_ext = '.nd2', op_ext = '.csv', pattern = r'(.+?).'):
+def getpendinglist(src_dir, op_dir, src_ext = '.nd2', op_ext = '.csv'):
+	"""
+	getpendinglist compares the files from src_dir and the accomplisjed file in op_dir, 
+	then creates a pending list of unprocessed image. 
+	"""
+	
+	srclist = listfiles(src_dir, src_ext)
+	oplist = listfiles(op_dir, op_ext)
+
+	oplist_basename = []
+	for i in oplist:
+		name = os.path.basename(i)
+		basename = os.path.splitext(name)[0]
+		oplist_basename.append(basename)
+	
+	pendingfllist = []
+	pendingpathlist_input = []
+	pendingpathlist_output = []
+	
+	for i in range(len(srclist)):
+		srcflname = os.path.basename(srclist[i])
+		srcflbasename = os.path.splitext(srcflname)[0]
+		
+		if not srcflbasename in oplist_basename:
+			pendingfllist.append(srcflbasename)
+			pendingpathlist_input.append(srclist[i])			
+			pendingpathlist_output.append(os.path.join(op_dir, srcflbasename + op_ext))
+			
+	return (pendingfllist, pendingpathlist_input, pendingpathlist_output)
+
+def getStatistics(imp):  
+	""" Return statistics for the given ImagePlus """  
+	options = IS.MEAN | IS.MEDIAN | IS.MIN_MAX
+	ip = imp.getProcessor()  
+	stats = IS.getStatistics(ip, options, imp.getCalibration())  
+	return stats.mean, stats.median, stats.min, stats.max  
+
+def garbagecollect(iteration = 3):
+	for i in range(iteration):
+		gc.collect()
+		
+# Functions Section Ends ----------------------------------------------------- #
+def run_script(path=path):
+	print('Preparing ...')
+	
+	import gc
+	path = str(path)
+	nchannels = 2
+	 
+	if batchmodeop:
+		Interpreter.batchMode = True
+	
+	ip_path = os.path.join(path, dir_output, ip_dir, ip_subdir)
+	op_path = os.path.join(path, dir_output, op_dir, op_subdir)
+	
+	dircheck(op_path)
+	
+	pendingfllist = []
+	pendingpathlist_ip = []
+	pendingpathlist_op = []
+
+	src_ext = '.csv'
+	op_ext = '.csv'
+
+	filename = listfiles(os.path.join(ip_path, '1'), '.csv')['filelist']
+	print(filename)
+	
+	for i in range(len(filename)):
+	# for i in range(1):
+		# input path
+		c1_filename = filename[i].replace('.tif', '.csv')
+		c2_filename = filename[i].replace('.tif', '.csv')
+		c1_path = os.path.join(ip_path, '1', c1_filename)
+		c2_path = os.path.join(ip_path, '2', c2_filename)
+		print(c1_path)
+		print(c2_path)
+
+		# output path
+		op_filename = filename[i].replace('.tif', '.csv')
+		op_path_temp = os.path.join(op_path, op_filename) 
+		print(op_path_temp)
+		
+		IJ.run("Import results", "filepath=[" + c1_path + "] fileformat=[CSV (comma separated)] livepreview=false rawimagestack= startingframe=1 append=false")
+		
+		IJ.run("Import ground-truth", "filepath=[" + c2_path + "] fileformat=[CSV (comma separated)] startingframe=1 append=false")
+		IJ.run("CBC", "addcbc=true radiusstep=50 addnncount=true channel1=[Results table] channel2=[Ground-truth table] stepcount=10 dimensions=2D addnndist=true")
+		IJ.run("Export ground-truth", ("filepath=[" + op_path_temp + "] "
+				"fileformat=[CSV (comma separated)] "
+				"sigma=true chi2=true offset=true "
+				"cbc=true " 
+				"x=true y=true id=true frame=true "
+				"nn_dist=true bkgstd=true "
+				"uncertainty=true "
+				"intensity=true " 
+				"neighbors_in_dist_50=true "
+				"neighbors_in_dist_100=true "
+				"neighbors_in_dist_150=true "
+				"neighbors_in_dist_200=true "
+				"neighbors_in_dist_250=true "
+				"neighbors_in_dist_300=true "
+				"neighbors_in_dist_350=true "
+				"neighbors_in_dist_400=true "
+				"neighbors_in_dist_450=true "
+				"neighbors_in_dist_500=true "))
+
+		imp = IJ.getImage()
+		imp.close()
+		imp = IJ.getImage()
+		imp.close()
+		
+
+if __name__ in ['__builtin__','__main__']:
+	run_script()
+	
+	# quit script after running the script
+	from java.lang import System
+	System.exit(0)
+	
--- plot.py
+++ plot.py
@@ -1,11 +1,13 @@
 # %%
 # make plot
 import os, sys
+import re
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.style
 import matplotlib as mpl
 mpl.style.use('default')
+
 
 # Functions Section Begins ----------------------------------------------------- #
 def dircheck(targetpaths):
@@ -23,6 +25,21 @@ def dircheck(targetpaths):
 			if not os.path.exists(path):
 				os.makedirs(path)
 
+def listfiles(path, extension = None):
+	filelist = []
+	fileabslist = []
+	for directory, dir_names, file_names in os.walk(path):
+		# print(file_names)
+		
+		for file_name in file_names:
+			if (not file_name.startswith('.')) & (file_name.endswith(extension)):
+				filepath_tmp =  os.path.join(directory, file_name + extension)
+				filelist.append(file_name)
+				fileabslist.append(filepath_tmp)
+	
+	return {'filelist': filelist,
+			'fileabslist': fileabslist}
+
 # Functions Section Ends ----------------------------------------------------- #
 # parameter
 nchannel= 2
@@ -38,14 +55,17 @@ ip_path = os.path.join(path, analysis_dir, spacialtestdir, csvdir)
 opK_dir = 'plot_K'
 opL_dir = 'plot_L'
 opH_dir = 'plot_H'
+optotal_dir = 'plot_total'
 
 dir_for_check = []
 opK_path = os.path.join(path, analysis_dir, spacialtestdir, opK_dir)
 opL_path = os.path.join(path, analysis_dir, spacialtestdir, opL_dir)
 opH_path = os.path.join(path, analysis_dir, spacialtestdir, opH_dir)
+optotal_path = os.path.join(path, analysis_dir, spacialtestdir, optotal_dir)
 dir_for_check.append(opK_path)
 dir_for_check.append(opL_path)
 dir_for_check.append(opH_path)
+dir_for_check.append(optotal_path)
 
 for i in range(nchannel):
     dir_tmp = os.path.join(opK_path, str(i+1))
@@ -59,59 +79,153 @@ print(dir_for_check)
 
 
 # %%
-filelist = []
-fileabslist = []
-outputfilelist_K = []
-outputfilelist_L = []
-outputfilelist_H = []
+filelist = {}
+
+filenamelist = listfiles(os.path.join(ip_path, '1'), '.csv')['filelist']
+# print(filenamelist)
+
+filedir = ['ip_filename', 'ip_path', 'plot_K', 'plot_L', 'plot_H']
+treatment = ['wildtype', 'knockout']
+channel = list(range(2))
+print(channel)
 
 # %%
-for c in range(nchannel):
-    for directory, dir_names, file_names in os.walk(ip_path):
-        for file_name in file_names:
-            if (not file_name.startswith('.')) & (file_name.endswith('.csv')):
-                filepath_tmp =  os.path.join(directory, file_name)
-                filelist.append(file_name)
-                fileabslist.append(filepath_tmp)
-                filenamepng = file_name.replace('.csv', '.png')
-                filepath_K_tmp = os.path.join(opK_path, str(c+1), filenamepng)
-                filepath_L_tmp = os.path.join(opL_path, str(c+1), filenamepng)
-                filepath_H_tmp = os.path.join(opH_path, str(c+1), filenamepng)
-                outputfilelist_K.append(filepath_K_tmp)
-                outputfilelist_L.append(filepath_L_tmp)
-                outputfilelist_H.append(filepath_H_tmp)
+for c in channel:
+    filelist[str(c+1)] = {}
+    # print(filelist)
+    for group in treatment:
+        # print(group)
+        filelist[str(c+1)][group] = {}    
+        
+        # create list
+        filelist_temp = []
+        for l in filenamelist: 
+            if group == 'wildtype':
+                x = re.search('(.*)_w{1}[0-9]{1}_(.*)', l)
+            else: 
+                x = re.search('(.*)_k{1}[0-9]{1}_(.*)', l) 
+            try: 
+                found = x.group(0)
+                filelist_temp.append(found)
+            except AttributeError:
+                found = ''
+        
+        # print(filelist_temp)
+
+        ip_filepath = []
+        op_path_K = []
+        op_path_L = []
+        op_path_H = []
+        
+        for f in filelist_temp: 
+            filepath_tmp =  os.path.join(ip_path, str(c+1), f)
+            ip_filepath.append(filepath_tmp)
+            # print(ip_filepath)
+
+            filename_tmp_png = f.replace('.csv', '.png')
+            op_path_K_tmp = os.path.join(opK_path, str(c+1), filename_tmp_png)
+            op_path_L_tmp = os.path.join(opL_path, str(c+1), filename_tmp_png)
+            op_path_H_tmp = os.path.join(opH_path, str(c+1), filename_tmp_png)
+            op_path_K.append(op_path_K_tmp)
+            op_path_L.append(op_path_L_tmp)
+            op_path_H.append(op_path_H_tmp)
+
+        filelist[str(c+1)][group][filedir[0]] = filelist_temp
+        filelist[str(c+1)][group][filedir[1]] = ip_filepath
+        filelist[str(c+1)][group][filedir[2]] = op_path_K
+        filelist[str(c+1)][group][filedir[3]] = op_path_L
+        filelist[str(c+1)][group][filedir[4]] = op_path_H
 
 print(filelist)
-print(fileabslist)
+# %%
+for key, value in filelist.items() :
+    print (key)
+    for key2, value2 in filelist[key].items():
+        print(key2)
+        for key3, value3 in filelist[key][key2].items():
+            print(key3)
 
 # %%
-for i in range(len(filelist)):
-# for i in range(1):
-    filepath = fileabslist[i]
-    print(filepath)
-    data = pd.read_csv(filepath, header=0)
-    
-    print(data)
+variable = ['K_r', 'L_r', 'H_r']
+data_list = []
+for c in channel:
+    for group in treatment:
+        for i in range(len(filelist[str(c+1)][group][filedir[0]])):
+        # for i in range(10):
+            filepath = filelist[str(c+1)][group][filedir[1]][i]
+            print(filepath)
+            data = pd.read_csv(filepath, header=0, index_col = 0)
+            data['filename'] = filelist[str(c+1)][group][filedir[0]][i]
+            data['group'] = group
+            data['channel'] = str(c+1)
+            data_list.append(data)
 
-    fig, axes = plt.subplots()
-    plt.plot(data['r'], data['K_r'])
-    fig.savefig(outputfilelist_K[i])
-    axes.set_xlim(0, 2500)
-    plt.close()
+            # print(data)
+            
+            for j in range(2, 5): 
+                fig, axes = plt.subplots()
+                plt.plot(data['r'], data[variable[j-2]])
+                fig.savefig(filelist[str(c+1)][group][filedir[j]][i])
+                axes.set_xlim(0, 2500)
+                plt.close()
+        
+        data_total = pd.concat(data_list, axis = 0)
 
-    fig, axes = plt.subplots()
-    plt.plot(data['r'], data['L_r'])
-    fig.savefig(outputfilelist_L[i])
-    axes.set_xlim(0, 2500)
-    plt.close()
+# %%
+print(data_total)
 
-    fig, axes = plt.subplots()
-    plt.plot(data['r'], data['H_r'])
-    fig.savefig(outputfilelist_H[i])
-    axes.set_xlim(0, 2500)
-    plt.close()
+# %%
+for c in channel:
+    print(c)
+    data_temp = data_total[data_total['channel'] == str(c+1)]
+    print(data_temp)
+    for j in range(2, 5):
+        print(variable[j-2])
+        fig, axes = plt.subplots()
+        colors = ['red', 'blue']
+        for m in range(len(treatment)):
+            for i in range(len(filelist[str(c+1)][treatment[m]][filedir[0]])):
+            # for i in range(10):
+                filename_tmp = filelist[str(c+1)][treatment[m]][filedir[0]][i]
+                print(filename_tmp)
+                data_plot = data_temp[data_temp['filename'] == filename_tmp]
+                print(data_plot)
+                plt.plot(data_plot['r'], data_plot[variable[j-2]], color = colors[m], alpha = 0.2)
+        
+        fig.savefig(os.path.join(optotal_path, variable[j-2] + '_c' +  str(c+1) + '.png'))
+        axes.set_xlim(0, 2500)
+        optotal_path
+        plt.close()
+# %%
+data_total = pd.DataFrame(data_total)
+display(data_total)
+# %%
+data_grouped_mean = data_total.groupby(by = ['channel', 'group', 'r']).mean()
+print(data_grouped_mean)
+data_grouped_sem = data_total.groupby(by = ['channel', 'group', 'r']).sem()
+print(data_grouped_sem)
+# %%
+for c in channel:
+    for var in variable:
+        fig, axes = plt.subplots()
+        colors = ['red', 'blue']
+        for m in range(len(treatment)):
+            data_mean_temp = data_grouped_mean.loc[str(c+1), treatment[m]]
+            x = data_mean_temp.reset_index()['r']
+            y = data_mean_temp.reset_index()[var]
+            data_sem_temp = data_grouped_sem.loc[str(c+1), treatment[m]]
+            yerr = data_sem_temp.reset_index()[var]
+            plt.errorbar(x, y, yerr = yerr, color = colors[m], alpha = 0.2)
 
-    
+        fig.savefig(os.path.join(optotal_path, var + '_mean_c' +  str(c+1) + '.png'))
+        axes.set_xlim(0, 2500)
+        optotal_path
+        plt.close()
+
+            
+
+print(data_x.reset_index()['r'])
 
 # %%
 print(1+1)
+
